284aa06d7004defaf64aec3721ee2002238b857c
hyunwook <c004245@naver.com>
2018-12-26 22:42:46 +0900

    [#162843575] Material-Calendar-Library 추가

--- app/src/main/java/com/playgilround/schedule/client/Activity/ScheCalendarActivity.java
+++ app/src/main/java/com/playgilround/schedule/client/Activity/ScheCalendarActivity.java
@@ -1,9 +1,15 @@
 package com.playgilround.schedule.client.Activity;
 
+import android.support.v7.app.AppCompatActivity;
+
+import com.applandeo.materialcalendarview.CalendarView;
+
 /**
  * 18-12-26
  * Schedule Calendar Activity
  * added by CHO
  */
-public class ScheCalendarActivity {
+public class ScheCalendarActivity extends AppCompatActivity {
+
+    private CalendarView calendarView;
 }
