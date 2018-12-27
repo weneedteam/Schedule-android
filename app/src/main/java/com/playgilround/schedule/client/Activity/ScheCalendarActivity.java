f18458ba39cfc3104e45c1ebfb4d291b417ae52d
hyunwook <c004245@naver.com>
2018-12-27 17:37:29 +0900

    [#162843575] Add Click Calendar listener

--- app/src/main/java/com/playgilround/schedule/client/Activity/ScheCalendarActivity.java
+++ app/src/main/java/com/playgilround/schedule/client/Activity/ScheCalendarActivity.java
@@ -2,6 +2,7 @@ package com.playgilround.schedule.client.Activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.widget.CalendarView;
 
@@ -27,11 +28,22 @@ public class ScheCalendarActivity extends AppCompatActivity {
         setTitle("스케줄 달력");
         calendarView = findViewById(R.id.calendarView);
 
+        //날짜 클릭 시 다이얼로그
+        calendarView.setOnDayClickListener(eventDay -> showDialogCalendar(eventDay.getCalendar().getTime().toString()));
+
         //다음 달로 이동
         calendarView.setOnForwardPageChangeListener(() -> Log.d(TAG, "Next Month ---"));
 
         //전 달로 이동
         calendarView.setOnPreviousPageChangeListener(() -> Log.d(TAG, "Previous Month ---"));
 
+    }
+
+    //show Dialog When User Click Calendar
+    private void showDialogCalendar(String date) {
+        Log.d(TAG, "date --> "  + date);
+        DisplayMetrics dm = getApplicationContext().getResources().getDisplayMetrics();
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
     }
 }
