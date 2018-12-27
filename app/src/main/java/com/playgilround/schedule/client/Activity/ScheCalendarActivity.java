0454e7b7db0752f0efc9dff49c8c2992fccfa93b
hyunwook <c004245@naver.com>
2018-12-27 18:14:16 +0900

    [#162843575] 캘린더 날짜 클릭 시, 클릭 날짜가 적힌 다이얼로그 표시

--- app/src/main/java/com/playgilround/schedule/client/Activity/ScheCalendarActivity.java
+++ app/src/main/java/com/playgilround/schedule/client/Activity/ScheCalendarActivity.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.WindowManager;
 import android.widget.CalendarView;
 
+import com.playgilround.schedule.client.Dialog.ScheCalendarDialog;
 import com.playgilround.schedule.client.R;
 
 
@@ -41,9 +43,72 @@ public class ScheCalendarActivity extends AppCompatActivity {
 
     //show Dialog When User Click Calendar
     private void showDialogCalendar(String date) {
-        Log.d(TAG, "date --> "  + date);
+        Log.d(TAG, "date --> " + date);
         DisplayMetrics dm = getApplicationContext().getResources().getDisplayMetrics();
         int width = dm.widthPixels;
         int height = dm.heightPixels;
+
+        String[] date_arr = date.split(" "); //공백 기준
+
+        String strYear = date_arr[5];
+        String strMonth = monthChange(date_arr[1]);
+        String strDay = date_arr[2];
+
+        String strDate = strYear + strMonth + strDay;
+
+        ScheCalendarDialog calendarDialog = new ScheCalendarDialog(this, strDate);
+
+        WindowManager.LayoutParams wm = calendarDialog.getWindow().getAttributes();
+
+        wm.copyFrom(calendarDialog.getWindow().getAttributes());
+
+        wm.width = (int) (width / 1.2);
+        wm.height = (int) (height / 1.5);
+
+        calendarDialog.show();
+    }
+
+    //Month Eng -> Num
+    private String monthChange(String month) {
+        String result = "";
+        switch (month) {
+            case "Jan":
+                result = "01";
+                break;
+            case "Feb":
+                result = "02";
+                break;
+            case "Mar":
+                result = "03";
+                break;
+            case "Apr":
+                result = "04";
+                break;
+            case "May":
+                result = "05";
+                break;
+            case "Jun":
+                result = "06";
+                break;
+            case "Jul":
+                result = "07";
+                break;
+            case "Aug":
+                result = "08";
+                break;
+            case "Sep":
+                result = "09";
+                break;
+            case "Oct":
+                result = "10";
+                break;
+            case "Nov":
+                result = "11";
+                break;
+            case "Dec":
+                result = "12";
+                break;
+        }
+        return result;
     }
 }
