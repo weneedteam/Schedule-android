566448205ce506a63b87fde6a2ee94b4556eda5d
hyunwook <c004245@naver.com>
2019-01-02 09:45:23 +0900

    [#162858104] 스케줄 화면 구성 작업 DisplayMetrics 사용

--- app/src/main/java/com/playgilround/schedule/client/activity/ScheduleInfoActivity.java
+++ app/src/main/java/com/playgilround/schedule/client/activity/ScheduleInfoActivity.java
@@ -7,9 +7,11 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.content.IntentCompat;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -54,6 +56,20 @@ public class ScheduleInfoActivity extends Activity implements View.OnClickListen
         tvDate.setText(strDate);
 
         findViewById(R.id.ivAddBtn).setOnClickListener(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        DisplayMetrics dm = getApplicationContext().getResources().getDisplayMetrics();
+                int width = dm.widthPixels;
+                int height = dm.heightPixels;
+
+        WindowManager.LayoutParams wm = getWindow().getAttributes();
+
+        wm.copyFrom(getWindow().getAttributes());
+        wm.width = (int) (width / 1.2);
+        wm.height = (int) (height / 1.5);
     }
     /*public ScheduleInfoActivity(Activity activity, Context context, String date) {
         super(context);
