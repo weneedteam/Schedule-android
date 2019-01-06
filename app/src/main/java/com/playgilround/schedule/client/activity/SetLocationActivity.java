504afbeb7c3f47263210f6e96703b27ebdf431d9
hyunwook <c004245@naver.com>
2019-01-06 17:19:39 +0900

    [#162892134] Google Map Key 추가 후, 화면 표시

--- app/src/main/java/com/playgilround/schedule/client/activity/SetLocationActivity.java
+++ app/src/main/java/com/playgilround/schedule/client/activity/SetLocationActivity.java
@@ -2,29 +2,40 @@ package com.playgilround.schedule.client.activity;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.playgilround.schedule.client.R;
 
 /**
  * 19-01-01
  * 위치 관련 Activity
  */
-public class SetLocationActivity extends Activity {
+public class SetLocationActivity extends Activity implements OnMapReadyCallback {
 
     double latitude; //위도
     double longitude; //경도
 
     static final String TAG = SetLocationActivity.class.getSimpleName();
 
+    private GoogleMap mMap;
+    private Geocoder geocoder;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_location);
 
         final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         try {
@@ -61,4 +72,31 @@ public class SetLocationActivity extends Activity {
 
         }
     };
+
+    @Override
+    public void onMapReady(final GoogleMap map) {
+        mMap = map;
+
+        geocoder = new Geocoder(this);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+
+        LatLng destMap = new LatLng(latitude, longitude);
+
+        markerOptions.position(destMap);
+
+        //반경 500M 원
+        CircleOptions circle = new CircleOptions().center(destMap)
+                .radius(500)     //반지름 단위 : m
+                .strokeWidth(0f) //선 없음
+                .fillColor(Color.parseColor("#880000ff")); //배경색
+        markerOptions.title("내 위치");
+        markerOptions.snippet("내 위치");
+        map.addMarker(markerOptions);
+        map.addCircle(circle);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(destMap, 15));
+        map.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+    }
+
 }
