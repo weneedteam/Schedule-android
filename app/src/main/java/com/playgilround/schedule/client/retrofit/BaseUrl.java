bd45b0a493089c0f68ba3853307103e6ff33311e
JoungSik <tjstlr2010@gmail.com>
2019-01-13 17:12:43 +0900

    [#163006884] SignUp API 연결

--- app/src/main/java/com/playgilround/schedule/client/retrofit/BaseUrl.java
+++ app/src/main/java/com/playgilround/schedule/client/retrofit/BaseUrl.java
@@ -2,6 +2,7 @@ package com.playgilround.schedule.client.retrofit;
 
 public class BaseUrl {
 
-    public static final String BASE_URL = "";
+    static final String BASE_URL = "http://schedule-prod.ap-northeast-2.elasticbeanstalk.com/api/";
 
+    static final String PATH_SIGN_UP = "accounts/signup";
 }
