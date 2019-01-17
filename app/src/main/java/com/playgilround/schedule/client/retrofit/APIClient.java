e3e51a0e183a558edf6308c256a6d8512b0e1385
JoungSik <tjstlr2010@gmail.com>
2019-01-18 01:55:23 +0900

    Add OKHttpClient Library and logging network

--- app/src/main/java/com/playgilround/schedule/client/retrofit/APIClient.java
+++ app/src/main/java/com/playgilround/schedule/client/retrofit/APIClient.java
@@ -1,5 +1,7 @@
 package com.playgilround.schedule.client.retrofit;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -9,6 +11,20 @@ public class APIClient {
         return new Retrofit.Builder()
                 .baseUrl(BaseUrl.BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    public static Retrofit getLoggingClient() {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
+
+        return new Retrofit.Builder()
+                .baseUrl(BaseUrl.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .build();
     }
 
