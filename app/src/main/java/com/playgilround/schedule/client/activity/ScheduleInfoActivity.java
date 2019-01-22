5448cdaf375f2540a46e917f8acbfd4787bb46f1
hyunwook <c004245@naver.com>
2019-01-23 00:01:53 +0900

    [#163385767] ScheduleInfoActivity.class ButterKnife 작업

--- app/src/main/java/com/playgilround/schedule/client/activity/ScheduleInfoActivity.java
+++ app/src/main/java/com/playgilround/schedule/client/activity/ScheduleInfoActivity.java
@@ -20,6 +20,9 @@ import java.util.ArrayList;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import io.realm.Realm;
 import io.realm.RealmResults;
 
@@ -27,16 +30,23 @@ import io.realm.RealmResults;
  * 18-12-27
  * Calendar 에서 날짜 클릭 시 스케줄 정보가 표시되는 액티비티
  */
-public class ScheduleInfoActivity extends Activity implements View.OnClickListener, ScheduleCardAdapter.Listener {
+public class ScheduleInfoActivity extends Activity implements ScheduleCardAdapter.Listener {
 
+    @BindView(R.id.recycler_view)
+    RecyclerView mRecyclerView;
+
+    @BindView(R.id.btn_cancel)
+    Button btnCancel;
+
+    @BindView(R.id.tv_date)
+    TextView tvDate;
 
     private String date;
     private String strDateDay;
-    
+
     static final String TAG = ScheduleInfoActivity.class.getSimpleName();
 
     public static final int ADD_SCHEDULE = 1000;
-    RecyclerView mRecyclerView;
     RecyclerView.LayoutManager mLayoutManager;
     ScheduleCardAdapter mAdapter;
     private ArrayList<ScheduleCard> arrCard;
@@ -52,20 +62,19 @@ public class ScheduleInfoActivity extends Activity implements View.OnClickListen
 
         setContentView(R.layout.dialog_calendar);
 
+        ButterKnife.bind(this);
         realm = Realm.getDefaultInstance();
 
-        mRecyclerView = findViewById(R.id.recycler_view);
         mRecyclerView.setHasFixedSize(true);
 
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
+        btnCancel.setOnClickListener(l -> finish());
+
         Intent intent = getIntent();
         date = intent.getStringExtra(INTENT_EXTRA_DATE);
-        Button cancel = findViewById(R.id.btn_cancel);
-        cancel.setOnClickListener(l -> finish());
 
-        TextView tvDate = findViewById(R.id.tv_date);
         String strYear = date.substring(0, 4);
         String strMonth = date.substring(4, 6);
         String strDay = date.substring(6, 8);
@@ -75,10 +84,15 @@ public class ScheduleInfoActivity extends Activity implements View.OnClickListen
 
         strDateDay = strYear + "-" + strMonth + "-" + strDay;
 
-        findViewById(R.id.ivAddBtn).setOnClickListener(this);
-
         getTodaySchedule(realm);
 
+    }
+
+    @OnClick(R.id.ivAddBtn)
+    void onAddScheduleClick() {
+        Intent intent = new Intent(this, AddScheduleActivity.class);
+        intent.putExtra("date", date);
+        startActivityForResult(intent, ADD_SCHEDULE);
     }
 
     @Override
@@ -93,16 +107,6 @@ public class ScheduleInfoActivity extends Activity implements View.OnClickListen
         wm.copyFrom(getWindow().getAttributes());
         wm.width = (int) (width / 1.2);
         wm.height = (int) (height / 1.5);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.ivAddBtn:
-                Intent intent = new Intent(this, AddScheduleActivity.class);
-                intent.putExtra("date", date);
-                startActivityForResult(intent, ADD_SCHEDULE);
-        }
     }
 
     //CardView Click
