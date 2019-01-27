65482b4ad14569a87d907e1ce39e9c152a0fa364
hyunwook <c004245@naver.com>
2019-01-27 16:00:06 +0900

    [#163423070] 디테일 카드뷰에 저장된 시간 표시, SIgnInActivity 로그인 버튼에 테스트로 ScheduleCalendarActivity Intent  연결

--- app/src/main/java/com/playgilround/schedule/client/singin/SignInActivity.java
+++ app/src/main/java/com/playgilround/schedule/client/singin/SignInActivity.java
@@ -1,10 +1,18 @@
 package com.playgilround.schedule.client.singin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.playgilround.schedule.client.R;
+import com.playgilround.schedule.client.activity.MainActivity;
+import com.playgilround.schedule.client.activity.ScheduleCalendarActivity;
+import com.playgilround.schedule.client.model.User;
 
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SignInActivity extends AppCompatActivity implements SignInContract.View {
 
@@ -14,8 +22,14 @@ public class SignInActivity extends AppCompatActivity implements SignInContract.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
 
         new SignInPresenter(this);
+    }
+
+    @OnClick(R.id.btn_login)
+    public void signIn() {
+        startActivity(new Intent(this, ScheduleCalendarActivity.class));
     }
 
     @Override
