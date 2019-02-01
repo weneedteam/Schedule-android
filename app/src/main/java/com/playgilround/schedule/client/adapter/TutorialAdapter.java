3891faa9670d5b2b54128c6ef0c2850e6e7524f8
hyunwook <c004245@naver.com>
2019-02-01 23:49:56 +0900

    [#163590448] tutorial 텍스트는 string.xml, image 관련 텍스트는 static final String 으로 대체.
    특정 글자만 색깔 변경되도록 수정

--- app/src/main/java/com/playgilround/schedule/client/adapter/TutorialAdapter.java
+++ app/src/main/java/com/playgilround/schedule/client/adapter/TutorialAdapter.java
@@ -1,6 +1,12 @@
 package com.playgilround.schedule.client.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +17,7 @@ import android.widget.Toast;
 import com.playgilround.schedule.client.R;
 import com.squareup.picasso.Picasso;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,28 +25,28 @@ import butterknife.OnClick;
 
 public class TutorialAdapter extends RecyclerView.Adapter<TutorialAdapter.ViewHolder> {
 
+    private static final String image1 = "tutorial";
+    private static final String image2 = "illustration";
 
-    private String[] images = new String[]{
-            "tutorial", "illustration", "illustration"};
-
-    private String[] texts = new String[]{
-            "일정을 등록할 때,\n 장소를 설정해주세요.", "장소에 가까워지면\n 도착 버튼이 활성화됩니다.", "도착 버튼을 누르고\n 친구와 함께 경쟁하세요."};
+    private String[] images  = new String[]{image1, image2, image2};
 
     private Context mContext;
 
+    int startText, lastText;
     public TutorialAdapter(Context context) {
         mContext = context;
     }
 
+    @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(
                 R.layout.item_tutorial, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.bindView(position);
     }
 
@@ -64,6 +71,12 @@ public class TutorialAdapter extends RecyclerView.Adapter<TutorialAdapter.ViewHo
 
         void bindView(int position) {
             String image = images[position];
+
+            String[] texts = new String[]{
+                    mContext.getString(R.string.text_tutorial_start1),
+                    mContext.getString(R.string.text_tutorial_start2),
+                    mContext.getString(R.string.text_tutorial_start3)};
+
             String text = texts[position];
 
             int resourceID = mContext.getResources().getIdentifier(image, "mipmap", mContext.getPackageName());
@@ -71,7 +84,21 @@ public class TutorialAdapter extends RecyclerView.Adapter<TutorialAdapter.ViewHo
             mImageView.setTag(position);
             Picasso.get().load(resourceID).into(mImageView);
 
-            mTextView.setText(text);
-        }
+            //특정 글자만 색깔 변경
+            SpannableStringBuilder ssb = new SpannableStringBuilder(text);
+            if (position == 0) {
+                startText = 12;
+                lastText = 14;
+            } else if (position == 1) {
+                startText = 10;
+                lastText = 12;
+            } else {
+                startText = 19;
+                lastText = 21;
+            }
+            ssb.setSpan(new ForegroundColorSpan(mContext.getResources().getColor(R.color.light_indigo)), startText, lastText, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+            mTextView.setText(ssb);
+            }
     }
 }
