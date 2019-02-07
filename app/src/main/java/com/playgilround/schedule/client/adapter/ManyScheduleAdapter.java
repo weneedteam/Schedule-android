19351bff160196a4dd47b8b2e84833c4d71e74ff
hyunwook <c004245@naver.com>
2019-02-07 23:53:09 +0900

    [#163680936] 스케줄 다중 선택 시, 유저에게 표시될 화면 구성

---
+++ app/src/main/java/com/playgilround/schedule/client/adapter/ManyScheduleAdapter.java
@@ -0,0 +1,8 @@
+package com.playgilround.schedule.client.adapter;
+
+/**
+ * 19-02-07
+ * 스케줄 다중 선택 어댑터
+ */
+public class ManyScheduleAdapter {
+}
--- app/src/main/java/com/playgilround/schedule/client/fragment/ManyScheduleFragment.java
+++ app/src/main/java/com/playgilround/schedule/client/fragment/ManyScheduleFragment.java
@@ -1,8 +1,16 @@
 package com.playgilround.schedule.client.fragment;
 
 import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.playgilround.schedule.client.R;
 
 import java.util.ArrayList;
+
+import javax.annotation.Nullable;
 
 /**
  * 19-02-07
@@ -18,5 +26,13 @@ public class ManyScheduleFragment extends DialogFragment {
         arrDay = day;
 
         return new ManyScheduleFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.recycler_many_schedule, container);
+
+        return rootView;
     }
 }
