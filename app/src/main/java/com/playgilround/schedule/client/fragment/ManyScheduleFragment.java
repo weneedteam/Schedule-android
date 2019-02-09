32eca26c84a109097b3a698840071e8d0743ea31
hyunwook <c004245@naver.com>
2019-02-10 01:21:35 +0900

    [#163680936] 다중 날짜 선택 화면 구성 작업 (스케줄 저장으로 넘어갈 경우 화면 재 구성해야될거같음)

--- app/src/main/java/com/playgilround/schedule/client/fragment/ManyScheduleFragment.java
+++ app/src/main/java/com/playgilround/schedule/client/fragment/ManyScheduleFragment.java
@@ -1,14 +1,17 @@
 package com.playgilround.schedule.client.fragment;
 
 import android.app.DialogFragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import com.playgilround.schedule.client.R;
+import com.playgilround.schedule.client.activity.AddScheduleActivity;
 import com.playgilround.schedule.client.adapter.ManyScheduleAdapter;
 
 import java.util.ArrayList;
@@ -26,9 +29,12 @@ import butterknife.ButterKnife;
  * 선택 한 날짜로 저장할 지 표시되는 DialogFragment
  */
 public class ManyScheduleFragment extends DialogFragment {
-    
+
     @BindView(R.id.mRecycler)
     RecyclerView mRecycler;
+
+    @BindView(R.id.btn_save)
+    Button btnSave;
 
     ManyScheduleAdapter adapter;
     static ArrayList<String> arrDay;
@@ -47,6 +53,13 @@ public class ManyScheduleFragment extends DialogFragment {
         mRecycler.setLayoutManager(new LinearLayoutManager(this.getActivity()));
         adapter = new ManyScheduleAdapter(arrDay);
         mRecycler.setAdapter(adapter);
+        String strDate ="2019-02-03";
+
+        btnSave.setOnClickListener(l -> {
+            Intent intent = new Intent(this.getActivity(), AddScheduleActivity.class);
+            intent.putExtra("date", strDate);
+            startActivity(intent);
+        });
 
         return rootView;
     }
