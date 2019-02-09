ce6c373c7ff7dd62d5abdae02cdc9bc3956d3d3f
hyunwook <c004245@naver.com>
2019-02-10 00:30:39 +0900

    [#163680936] 스케줄 다중 선택 시, 날짜 화면에 표시되는 어댑터 구성

--- app/src/main/java/com/playgilround/schedule/client/fragment/ManyScheduleFragment.java
+++ app/src/main/java/com/playgilround/schedule/client/fragment/ManyScheduleFragment.java
@@ -2,15 +2,23 @@ package com.playgilround.schedule.client.fragment;
 
 import android.app.DialogFragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 
 import com.playgilround.schedule.client.R;
+import com.playgilround.schedule.client.adapter.ManyScheduleAdapter;
 
 import java.util.ArrayList;
 
 import javax.annotation.Nullable;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * 19-02-07
@@ -18,11 +26,13 @@ import javax.annotation.Nullable;
  * 선택 한 날짜로 저장할 지 표시되는 DialogFragment
  */
 public class ManyScheduleFragment extends DialogFragment {
+    
+    @BindView(R.id.mRecycler)
+    RecyclerView mRecycler;
 
-    static final String TAG = ManyScheduleFragment.class.getSimpleName();
-
-    static ArrayList arrDay;
-    public static ManyScheduleFragment getInstance(ArrayList day) {
+    ManyScheduleAdapter adapter;
+    static ArrayList<String> arrDay;
+    public static ManyScheduleFragment getInstance(ArrayList<String> day) {
         arrDay = day;
 
         return new ManyScheduleFragment();
@@ -32,7 +42,18 @@ public class ManyScheduleFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.recycler_many_schedule, container);
+        ButterKnife.bind(this, rootView);
+
+        mRecycler.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+        adapter = new ManyScheduleAdapter(arrDay);
+        mRecycler.setAdapter(adapter);
 
         return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        getDialog().getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
     }
 }
