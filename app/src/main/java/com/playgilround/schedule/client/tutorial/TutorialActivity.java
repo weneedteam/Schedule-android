f69861bacf240590fe3ff24da1b824dbdefd5311
hyunwook <c004245@naver.com>
2019-03-03 01:29:17 +0900

    [#163680540] 하단 여백 이미지 있던 부분, 디자이너 한테 다시받아서 적용함

--- app/src/main/java/com/playgilround/schedule/client/tutorial/TutorialActivity.java
+++ app/src/main/java/com/playgilround/schedule/client/tutorial/TutorialActivity.java
@@ -61,7 +61,7 @@ public class TutorialActivity extends AppCompatActivity implements TutorialContr
 
                 retPosition = mLinear.findFirstVisibleItemPosition();
                 if (adapter.getItemCount() -1 == retPosition) {
-                    mImageNext.setImageResource(R.mipmap.check_btn);
+                    mImageNext.setImageResource(R.mipmap.check);
                 } else {
                     mImageNext.setImageResource(R.mipmap.next_btn);
                 }
@@ -80,10 +80,10 @@ public class TutorialActivity extends AppCompatActivity implements TutorialContr
             overridePendingTransition(R.anim.enter, R.anim.exit);
             finish();
         } else if (adapter.getItemCount() -2 == retPosition) {
-            mImageNext.setImageResource(R.mipmap.check_btn);
+            mImageNext.setImageResource(R.mipmap.check);
             mRecyclerView.smoothScrollToPosition(retPosition +1);
         } else {
-            mImageNext.setImageResource(R.mipmap.next_btn);
+            mImageNext.setImageResource(R.mipmap.tutorial_btn);
             mRecyclerView.smoothScrollToPosition(retPosition +1);
         }
     }
