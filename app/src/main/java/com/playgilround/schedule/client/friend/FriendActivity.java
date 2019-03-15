9b3dac2408d849cf94d6ee9da93158c075d060a4
hyunwookcho <c004245@naver.com>
2019-03-15 18:42:34 +0900

    [#164524308] 내 프로필 클릭 시 프로필 정보 표시되는 프래그먼트 연결

--- app/src/main/java/com/playgilround/schedule/client/friend/FriendActivity.java
+++ app/src/main/java/com/playgilround/schedule/client/friend/FriendActivity.java
@@ -1,12 +1,13 @@
 package com.playgilround.schedule.client.friend;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ImageView;
 
 import com.playgilround.schedule.client.R;
 import com.playgilround.schedule.client.friend.view.FriendAdapter;
+import com.playgilround.schedule.client.profile.ProfileFragment;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -47,7 +48,11 @@ public class FriendActivity extends Activity implements FriendContract.View {
         mAdapter = new FriendAdapter(this);
         mRecyclerView.setAdapter(mAdapter);
 
-        ivMyProfile.setOnClickListener(l -> Log.d(TAG, "My Profile Click"));
+        ivMyProfile.setOnClickListener(l -> {
+            final ProfileFragment fragment = ProfileFragment.getInstance();
+            final FragmentManager fm = getFragmentManager();
+            fragment.show(fm, "TAG");
+        });
     }
 
     @Override
