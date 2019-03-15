ffbf872f3fb5cddcc45e13b8c87ba85407cf4086
hyunwookcho <c004245@naver.com>
2019-03-15 18:07:04 +0900

    [#164524308] 프로필 UI 작업 xml

---
+++ app/src/main/java/com/playgilround/schedule/client/profile/ProfileFragment.java
@@ -0,0 +1,11 @@
+package com.playgilround.schedule.client.profile;
+
+import android.app.Fragment;
+
+/**
+ * 19-03-15
+ * 나(친구) 프로필 관련 Fragment
+ */
+public class ProfileFragment extends Fragment {
+
+}
