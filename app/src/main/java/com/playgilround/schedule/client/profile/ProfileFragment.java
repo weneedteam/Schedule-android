aa8aa431277864b51fe18fea6bcbb2763cdbfef3
hyunwook <c004245@naver.com>
2019-03-17 13:59:37 +0900

    [#164524308] Profile FloatingButton 생성

--- app/src/main/java/com/playgilround/schedule/client/profile/ProfileFragment.java
+++ app/src/main/java/com/playgilround/schedule/client/profile/ProfileFragment.java
@@ -7,9 +7,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.playgilround.schedule.client.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -22,13 +26,50 @@ public class ProfileFragment extends DialogFragment {
         return new ProfileFragment();
     }
 
+    @BindView(R.id.fabTop)
+    FloatingActionButton fabTop;
+
+    @BindView(R.id.fabMiddle)
+    FloatingActionButton fabMiddle;
+
+    @BindView(R.id.fabBottom)
+    FloatingActionButton fabBottom;
+
+    private Animation animOpen, animClose;
+
+    private Boolean isFabOpen = false;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_profile, container);
         ButterKnife.bind(this, rootView);
 
+        animOpen = AnimationUtils.loadAnimation(getContext(), R.anim.fab_open);
+        animClose = AnimationUtils.loadAnimation(getContext(), R.anim.fab_close);
+
+        fabBottom.setOnClickListener(l -> floatingAnim());
+
         return rootView;
+    }
+
+    public void floatingAnim() {
+       if (isFabOpen) {
+           fabTop.startAnimation(animClose);
+           fabMiddle.startAnimation(animClose);
+
+           fabTop.setClickable(false);
+           fabMiddle.setClickable(false);
+           isFabOpen = false;
+       } else {
+           fabTop.startAnimation(animOpen);
+           fabMiddle.startAnimation(animOpen);
+
+           fabTop.setClickable(true);
+           fabMiddle.setClickable(true);
+           isFabOpen = true;
+       }
     }
 
     @Override
