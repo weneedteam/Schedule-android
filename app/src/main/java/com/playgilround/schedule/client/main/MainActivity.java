f6ad8ad5e2741a47baa2f7a17b0c708812a4806c
hyunwook <c004245@naver.com>
2019-08-04 17:30:53 +0900

    [#167686801] ViewPager + TabLayout 구성

--- app/src/main/java/com/playgilround/schedule/client/main/MainActivity.java
+++ app/src/main/java/com/playgilround/schedule/client/main/MainActivity.java
@@ -1,10 +1,13 @@
 package com.playgilround.schedule.client.main;
 
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TableLayout;
 
 import com.google.android.material.tabs.TabLayout;
@@ -25,17 +28,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements MainContract.View {
 
-    private static final String TAG = MainActivity.class.getSimpleName();
-
     @BindView(R.id.view_pager_main)
-    CustomViewPager mViewPager;
+    ViewPager mViewPager;
 
     @BindView(R.id.nav_main)
     TabLayout mTabLayout;
@@ -49,23 +49,19 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
         ButterKnife.bind(this);
         new MainPresenter(this);
 
+        ViewPagerAdapter pagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
 
-        tabChangeListener(0);
-
-//        setUpViewPager();
+        pagerAdapter.addFragment(R.drawable.image_nav_bar_friend, new FriendFragment());
+        pagerAdapter.addFragment(R.drawable.image_nav_bar_chat, new ChatFragment());
+        pagerAdapter.addFragment(R.drawable.image_nav_bar_calendar, new CalendarScheduleFragment());
+        pagerAdapter.addFragment(R.drawable.image_nav_bar_map, new MapFragment());
+        pagerAdapter.addFragment(R.drawable.image_nav_bar_setting, new SettingFragment());
+        mViewPager.setAdapter(pagerAdapter);
 
         mTabLayout.setupWithViewPager(mViewPager);
-        setUpTabLayout();
-    }
 
-    private View.OnClickListener setTabChangeListener(final int position) {
-        return v -> tabChangeListener(position);
-    }
-
-    public void tabChangeListener(int position) {
-//        tabChangeAction(position);
-        if (position != mViewPager.getCurrentItem()) {
-            mViewPager.setCurrentItem(position, false);
+        for (int i =0; i< mViewPager.getAdapter().getCount(); i++) {
+            mTabLayout.getTabAt(i).setIcon(pagerAdapter.mImageList.get(i));
         }
     }
     @Override
@@ -73,81 +69,11 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
         mPresenter = presenter;
     }
 
-    private void setUpTabLayout() {
-
-        TabLayout tabLayout = findViewById(R.id.nav_main);
-//        tabLayout.addTab(tabLayout.newTab().setText("첫번째"));
-//        tabLayout.addTab(tabLayout.newTab().setText("두번째"));
-//        tabLayout.addTab(tabLayout.newTab().setText("세번째"));
-//        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-
-        ViewGroup.LayoutParams layoutParams = new TableLayout.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-
-        View friendView = LayoutInflater.from(this).inflate(R.layout.item_tab_layout, null);
-        friendView.findViewById(R.id.image_tab).setBackgroundResource(R.drawable.image_nav_bar_friend);
-        friendView.setLayoutParams(layoutParams);
-        tabLayout.getTabAt(0).setCustomView(friendView);
-
-        View chatView = LayoutInflater.from(this).inflate(R.layout.item_tab_layout, null);
-        chatView.findViewById(R.id.image_tab).setBackgroundResource(R.drawable.image_nav_bar_chat);
-        chatView.setLayoutParams(layoutParams);
-        tabLayout.getTabAt(1).setCustomView(chatView);
-
-        View calendarView = LayoutInflater.from(this).inflate(R.layout.item_tab_layout, null);
-        calendarView.findViewById(R.id.image_tab).setBackgroundResource(R.drawable.image_nav_bar_calendar);
-        calendarView.setLayoutParams(layoutParams);
-        tabLayout.getTabAt(2).setCustomView(calendarView);
-
-        View mapView = LayoutInflater.from(this).inflate(R.layout.item_tab_layout, null);
-        mapView.findViewById(R.id.image_tab).setBackgroundResource(R.drawable.image_nav_bar_map);
-        mapView.setLayoutParams(layoutParams);
-        tabLayout.getTabAt(3).setCustomView(mapView);
-
-        View settingView = LayoutInflater.from(this).inflate(R.layout.item_tab_layout, null);
-        settingView.findViewById(R.id.image_tab).setBackgroundResource(R.drawable.image_nav_bar_setting);
-        settingView.setLayoutParams(layoutParams);
-        tabLayout.getTabAt(4).setCustomView(settingView);
-
-        PagerAdapter adapter = new com.playgilround.schedule.client.calendar.util.PagerAdapter(getSupportFragmentManager(), mTabLayout.getTabCount());
-        mViewPager.setAdapter(adapter);
-        mViewPager.setPagingEnabled();
-        mViewPager.setOffscreenPageLimit(5);
-        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
-        mTabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                mViewPager.setCurrentItem(tab.getPosition());
-                Log.d(TAG, "selected");
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
-    }
-
-    private void setUpViewPager() {
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        // Todo:: Add Fragment.
-        adapter.addFragment(new FriendFragment());
-        adapter.addFragment(new ChatFragment());
-        adapter.addFragment(new CalendarScheduleFragment());
-        adapter.addFragment(new MapFragment());
-        adapter.addFragment(new SettingFragment());
-        mViewPager.setAdapter(adapter);
-    }
 
     class ViewPagerAdapter extends FragmentPagerAdapter {
 
         private List<Fragment> mList = new ArrayList<>();
+        private List<Integer> mImageList = new ArrayList<>();
 
         ViewPagerAdapter(@NonNull FragmentManager fm) {
             super(fm);
@@ -162,9 +88,11 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
         @Override
         public int getCount() {
             return mList.size();
+
         }
 
-        public void addFragment(Fragment fragment) {
+        void addFragment(int drawable, Fragment fragment) {
+            mImageList.add(drawable);
             mList.add(fragment);
         }
     }
