c2ff703eb0aa8985d3a4a93ab6116f8acecc23ca
hyunwook <c004245@naver.com>
2019-08-07 14:49:27 +0900

    [#167748862] Application 단 Scope모듈 추가

---
+++ app/src/main/java/com/playgilround/schedule/client/di/AppModule.kt
@@ -0,0 +1,21 @@
+package com.playgilround.schedule.client.di
+
+import android.app.Application
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+/**
+ * 19-08-07
+ * Application Scope를 따름
+ */
+@Module
+class AppModule constructor(private var application: Application) {
+
+    @Provides
+    @Singleton
+    fun provideApplication(): Application {
+        return application
+    }
+}
