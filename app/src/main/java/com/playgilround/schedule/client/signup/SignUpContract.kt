3fbbc74c720b402c435244fda5b2093367a492d0
hyunwook <c004245@naver.com>
2019-08-08 11:17:13 +0900

    [#162826996] SignUp Kotlin

---
+++ app/src/main/java/com/playgilround/schedule/client/signup/SignUpContract.kt
@@ -0,0 +1,23 @@
+package com.playgilround.schedule.client.signup
+
+import com.playgilround.schedule.client.base.BasePresenter
+import com.playgilround.schedule.client.base.BaseView
+
+interface SignUpContract {
+
+    interface View: BaseView<Presenter> {
+        fun fieldCheck(check: Boolean)
+
+        fun signUpError(status: Int)
+
+        fun signUpComplete()
+    }
+
+    interface Presenter: BasePresenter {
+        fun onClickNext(position: Int)
+
+        fun onClickBack(position: Int)
+
+        fun signUp()
+    }
+}
