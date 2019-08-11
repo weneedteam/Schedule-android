f9625e7c9bd23204f738b9fe199748bec2da39a4
hyunwook <c004245@naver.com>
2019-08-11 14:41:03 +0900

    [#164851546] Schedule 부분 Adapter 연결

--- app/src/main/java/com/playgilround/schedule/client/calendar/adapter/ScheduleAdapter.kt
+++ app/src/main/java/com/playgilround/schedule/client/calendar/adapter/ScheduleAdapter.kt
@@ -1,2 +1,31 @@
 package com.playgilround.schedule.client.calendar.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.playgilround.schedule.client.R
+
+class ScheduleAdapter constructor(context: Context): RecyclerView.Adapter<ScheduleAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_schedule, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindView(position)
+    }
+
+    override fun getItemCount(): Int {
+        return 10 //test
+    }
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        fun bindView(position: Int) {
+
+        }
+    }
+
+
+}
