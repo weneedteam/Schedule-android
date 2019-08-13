d3cdf85473016e466276b2d750674449678fdd90
hyunwook <c004245@naver.com>
2019-08-13 17:38:11 +0900

    [#164697531] Schedule Repository 연결

---
+++ app/src/main/java/com/playgilround/schedule/client/data/source/network/ScheduleRemoteDataSource.kt
@@ -0,0 +1,8 @@
+package com.playgilround.schedule.client.data.source.network
+
+import android.content.Context
+import com.playgilround.schedule.client.data.source.ScheduleDataSource
+
+class ScheduleRemoteDataSource constructor(val context: Context) : ScheduleDataSource {
+
+}
