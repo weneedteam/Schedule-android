f58afcc1d2ea35824b9dece48f1ef2155e8b9009
hyunwook <c004245@naver.com>
2019-08-18 23:33:09 +0900

    [#167960367] Rx Retrofit 사용을 위해 Utils 기능추가

--- app/src/main/java/com/playgilround/schedule/client/util/ConnectivityInterceptor.kt
+++ app/src/main/java/com/playgilround/schedule/client/util/ConnectivityInterceptor.kt
@@ -3,10 +3,16 @@ package com.playgilround.schedule.client.util
 import android.content.Context
 import okhttp3.Interceptor
 import okhttp3.Response
+import java.io.IOException
 
 class ConnectivityInterceptor(private val mContext: Context): Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
-        if
+        if (!HttpUtils.isOnline(mContext)) {
+            throw IOException()
+        }
+
+        val builder = chain.request().newBuilder()
+        return chain.proceed(builder.build())
     }
 }
--- app/src/main/java/com/playgilround/schedule/client/util/HttpUtils.kt
+++ app/src/main/java/com/playgilround/schedule/client/util/HttpUtils.kt
@@ -1,14 +1,19 @@
 package com.playgilround.schedule.client.util
 
+import android.content.Context
+import android.net.ConnectivityManager
+import com.google.gson.GsonBuilder
 import com.playgilround.schedule.client.BuildConfig
-import com.playgilround.schedule.client.ScheduleApplication
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
-class HttpUtils {
-    internal val API_BASE_URL = "http://192.168.150.11:8000/"
+class HttpUtils constructor(private val mContext: Context) {
+    private val API_BASE_URL = "http://192.168.150.11:8000/"
 
     private var retrofitKey: Retrofit? = null
 
@@ -19,7 +24,7 @@ class HttpUtils {
         return retrofitKey
     }
 
-    fun provideClientKey(): OkHttpClient {
+    private fun provideClientKey(): OkHttpClient {
 
         val interceptor: Interceptor
 
@@ -27,12 +32,43 @@ class HttpUtils {
             interceptor = HttpLoggingInterceptor()
             interceptor.level = HttpLoggingInterceptor.Level.BODY
         } else {
-            interceptor = Connectivit
+            interceptor = ConnectivityInterceptor(mContext)
         }
+
+        return OkHttpClient.Builder().addInterceptor(interceptor).addInterceptor { chain ->
+            var request = chain.request()
+            val url = request.url().newBuilder().build()
+            request = request.newBuilder().url(url).build()
+            chain.proceed(request)
+        }.connectTimeout(30, TimeUnit.SECONDS).readTimeout(30, TimeUnit.SECONDS).build()
+    }
+
+    private fun provideRetrofit(baseURL: String, client: OkHttpClient): Retrofit {
+        val gson = GsonBuilder().setLenient().create()
+        return Retrofit.Builder()
+                .baseUrl(baseURL)
+                .client(client)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
     }
 
    companion object {
-       val connectivityManager =
+
+       private var INSTANCE: HttpUtils? = null
+
+       fun isOnline(context: Context): Boolean {
+           val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+           val networkInfo = connectivityManager.activeNetworkInfo
+           return (networkInfo != null && networkInfo.isConnected)
+       }
+
+       fun getInstance(context: Context): HttpUtils {
+           if (null == INSTANCE) {
+               INSTANCE = HttpUtils(context)
+           }
+           return INSTANCE as HttpUtils
+       }
    }
 
 
